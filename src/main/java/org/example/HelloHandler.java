e205ebd98269ac454ed2af894f56e88fb5d0ab5b
cotnetavkhelidzed-code <cotnetavkhelidzed@gmail.com>
2026-03-25 16:56:07 +0400

    web server

---
+++ src/main/java/org/example/HelloHandler.java
@@ -0,0 +1,20 @@
+import com.sun.net.httpserver.HttpExchange;
+import com.sun.net.httpserver.HttpHandler;
+
+import java.io.IOException;
+
+public class HelloHandler implements HttpHandler {
+
+    @Override
+    public void handle(HttpExchange exchange) throws IOException {
+
+        String response = "Hello from Http server";
+
+        exchange.sendResponseHeaders(200, response.length());
+
+        exchange.getResponseBody().write(response.getBytes());
+
+        exchange.getResponseBody().close();
+
+    }
+}
