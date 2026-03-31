fe100a9d4b366c74e51980f4c9d9ae372678639d
cotnetavkhelidzed-code <cotnetavkhelidzed@gmail.com>
2026-03-31 17:09:35 +0400

    server and client

--- src/main/java/org/example/PupilHandler.java
+++ src/main/java/org/example/PupilHandler.java
@@ -1,15 +1,18 @@
 package org.example;
 
-import com.fasterxml.jackson.core.PrettyPrinter;
 import com.fasterxml.jackson.databind.ObjectMapper;
 import com.sun.net.httpserver.HttpExchange;
 import com.sun.net.httpserver.HttpHandler;
 
 import java.io.IOException;
+import java.io.InputStream;
 import java.io.OutputStream;
+import java.nio.charset.StandardCharsets;
 import java.util.List;
+import java.util.Map;
+import java.util.stream.Collectors;
 
-public class PupilHandler implements HttpHandler {
+public class PupilHandler extends PupilRepository implements HttpHandler {
     private final PupilRepository pupilRepository;
     private final ObjectMapper objectMapper;
 
@@ -20,12 +23,27 @@ public class PupilHandler implements HttpHandler {
 
     @Override
     public void handle(HttpExchange exchange) throws IOException {
+        List<Pupil> pupils = pupilRepository.getAllpupils();
 
+
+        String query = exchange.getRequestURI().getRawQuery();
         if (!exchange.getRequestMethod().equalsIgnoreCase("GET")) {
             exchange.sendResponseHeaders(405, -1);
             return;
+        }else {
+            String name = query.substring(5);
+            System.out.println(name);
+            pupils = pupils.stream()
+                    .filter(pupil-> pupil.getName().equalsIgnoreCase(name))
+                    .collect(Collectors.toList());
+
         }
-        List<Pupil> pupils = pupilRepository.getAllpupils();
+
+//        if (query != null && query.contains("name=")){
+//
+//
+//        }
+
 
         String jsonResponse = objectMapper.writeValueAsString(pupils);
 
@@ -40,5 +58,11 @@ public class PupilHandler implements HttpHandler {
 
     }
 
+    public void handlePost(HttpExchange exchange)throws IOException{
+
+        Pupil newPupil = objectMapper.readValue(exchange.getRequestBody(), Pupil.class);
+
+
+    }
 
 }
--- src/main/java/org/example/PupilRepository.java
+++ src/main/java/org/example/PupilRepository.java
@@ -4,14 +4,21 @@ import java.util.ArrayList;
 import java.util.List;
 
 public class PupilRepository {
-    List<Pupil> getAllpupils(){
-        List<Pupil> pupils = new ArrayList<>();
+    List<Pupil> pupils = new ArrayList<>();
 
-        pupils.add(new Pupil("cotne tavkhelidze",19,91));
-        pupils.add(new Pupil("luka burjanadze",21,98));
-        pupils.add(new Pupil("erekle tavkhelidze",21,34));
+    List<Pupil> getAllpupils(){
+
+
+        pupils.add(new Pupil("cotne",19,91));
+        pupils.add(new Pupil("luka",21,98));
+        pupils.add(new Pupil("erekle",21,34));
 
         return pupils;
+
+    }
+
+    public void addPupil(Pupil pupil){
+        pupils.add(pupil);
     }
 
 
